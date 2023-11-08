1c5620c366b437f7137c70c6220f1a0af4e35a69
juanrozz <145585669+juanrozz@users.noreply.github.com>
2023-11-08 10:16:10 -0500

    Add files via upload

---
+++ src/test/java/com/javacrud/demo/JavaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
